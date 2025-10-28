2f4e8c3a9d4ced4a31da642c0a5bcfc6343deb69
Hassan Raza <hr.raza04@gmail.com>
2025-10-28 18:56:29 +0530

    dashboard statusCard

--- Admin-DashBoard/src/components/Dashboard/Grid.jsx
+++ Admin-DashBoard/src/components/Dashboard/Grid.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import StatsCards from './StatsCards'
 
 const Grid = () => {
   return (
     <div
-    className='px-4 grid gap-3 grid-cols-12'>Grid</div>
+    className='px-4 grid gap-3 grid-cols-12'>
+        <StatsCards/>
+    </div>
   )
 }
 
--- Admin-DashBoard/src/components/SideBar/RouteSelect.jsx
+++ Admin-DashBoard/src/components/SideBar/RouteSelect.jsx
@@ -20,7 +20,7 @@ const Route = ({
     title,
 })=>{
     return(
-         <button className={`flex items-center justify-start gap-2 w-full rounded px-2 py-1.5 text-sm transition-[box-shadow,_background-color,_color] ${
+         <button className={`flex items-center justify-start gap-2 w-full rounded px-2 py-1.5 text-sm transition-[box-shadow,background-color,color] ${
          selected ? "bg-white text-stone-950 shadow" :"hover:bg-stone-200 bg-transparent text-stone-500 shadow-none"}`}>
    <Icon className={selected ? "text-violet-500" : ""}/>
    <span>{title}</span>
